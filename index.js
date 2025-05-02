2f4eafd6773fda7b87113e01b3eeba1be9eb7ab5
AbdullahAlFalah <salem908mk@gmail.com>
2025-05-02 14:52:15 +0300

    Deploy_script_fix_7

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ app.get('/health', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
