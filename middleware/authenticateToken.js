f70c7a01fefc0d3db4272f1e11107107fe782c78
AbdullahAlFalah <salem908mk@gmail.com>
2025-05-24 18:17:49 +0300

    Debugging_Middleware

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -2,18 +2,26 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
 
 // Middleware to protect routes
-const authenticateToken = (req, res, next) => {
+const authenticateToken = (req, res, err, next) => {
   
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
+  if (res.status===500) {
+    return res.status(500).json({ ServerNote: 'Internal server error... Auth_Header: ', authHeader });
+  }
+
+  if (err) {
+    return res.status(502).json({ ServerNote: 'Error in authentication middleware: ', err });
+  }
+
   if (!token) {
-    return res.status(401).json({ ServerNote: 'Access denied. No token provided.' });
+    return res.status(401).json({ ServerNote: 'Access denied. No token provided... Secret_Key: ', SECRET_KEY });
   }
 
   jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
-      return res.status(403).json({ ServerNote: 'Invalid token.' });
+      return res.status(403).json({ ServerNote: 'Invalid token: ', token });
     }
     req.user = user; // Attach user info to the request
     next();
