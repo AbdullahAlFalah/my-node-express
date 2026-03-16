f2e2f307227c0d8acc1c14a085df8caba842ed70
AbdullahAlFalah <salem908mk@gmail.com>
2026-03-16 20:04:20 +0200

    debug 2

--- api/index.js
+++ api/index.js
@@ -144,7 +144,7 @@ app.post(`/api/users/login`, async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, user.password);      
     if (!isPasswordValid) { // user.password !== password can be used if password is not hashed
       console.log(`[AUTH] Invalid password attempt for: ${email}`); // ADD THIS FOR VERCEL LOGS
-      return res.status(401).json({ServerNote: 'Invalid password!'}); // 401 Unauthorized: The request has not been applied because it lacks valid authentication credentials for the target resource.
+      return res.status(200).json({ServerNote: 'Invalid password!'}); // 401 Unauthorized: The request has not been applied because it lacks valid authentication credentials for the target resource.
     }
 
     // Generate a JWT token
